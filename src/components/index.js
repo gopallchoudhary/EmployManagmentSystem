ca3f1c3e4a366ea89ebfc5c18396d2dfacbde058
gopallchoudhary <gopalchoudhary521@gmail.com>
2024-10-22 17:25:29 +0530

    added import index.js

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import Login from "./Auth/Login";
+import AdminDashboard from "./Dashboard/AdminDashboard";
+import EmployeeDashboard from "./Dashboard/EmployeeDashboard";
+import AcceptTask from "./TaskList/AcceptTask";
+import FailedTask from "./TaskList/FailedTask";
+import NewTask from "./TaskList/NewTask";
+import CompleteTask from "./TaskList/CompleteTask";
+
+export {
+  Login,
+  AcceptTask,
+  FailedTask,
+  NewTask,
+  CompleteTask,
+  AdminDashboard,
+  EmployeeDashboard,
+};
