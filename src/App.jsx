a7f801f0cc8f9ecdc7fc55de6a2a253186ebbc29
gopallchoudhary <gopalchoudhary521@gmail.com>
2024-10-22 16:44:52 +0530

    added initial folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Login from "./components/Auth/Login";
 
 function App() {
-  const [count, setCount] = useState(0);
+
 
   return (
     <>
-      <h1 className="text-8xl">Gopal Choudhary</h1>
+      <Login/>
     </>
   );
 }
