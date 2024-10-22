8979dc45e2069139c597a23765c9ec466e139770
gopallchoudhary <gopalchoudhary521@gmail.com>
2024-10-22 17:27:12 +0530

    added Log In

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import Login from "./components/Auth/Login";
+import { Login } from "./components";
 
 function App() {
 
--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,11 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setEmail("");
+    setPassword("");
+  };
+  
   return (
-    <div>
-      Login
+    <div className="h-screen w-screen flex items-center justify-center">
+      <div className="border-2 border-emerald-600 p-20 rounded-xl">
+        <form
+          onSubmit={(e) => {
+            submitHandler(e);
+          }}
+          className="flex items-center justify-center flex-col"
+        >
+          <input
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="border-2  border-emerald-500 px-5 py-4 rounded-full bg-transparent placeholder:text-gray-400 outline-none"
+            type="email"
+            placeholder="Enter your email"
+          />
+          <input
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="border-2 mt-4 border-emerald-500 px-5 py-4 rounded-full bg-transparent placeholder:text-gray-400 outline-none"
+            type="password"
+            placeholder="Enter your password"
+          />
+          <button className="border-2 mt-4 w-full border-emerald-500 px-5 py-4 rounded-full bg-emerald-500 placeholder:text-gray-400 outline-none">
+            Log In
+          </button>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Login;
