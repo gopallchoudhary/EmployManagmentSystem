5668ace7edf2a21b5bf7fe49711e9c9600b6e041
gopallchoudhary <gopalchoudhary521@gmail.com>
2024-11-17 08:00:14 +0530

    remove the stric mode

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,9 @@ import App from "./App.jsx";
 import "./index.css";
 import AuthProvider from "./context/AuthProvider.jsx";
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  //<StrictMode>
     <AuthProvider>
       <App />
     </AuthProvider>
-  </StrictMode>
+  //</StrictMode>
 );
